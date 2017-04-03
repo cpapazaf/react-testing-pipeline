3e7b5422325119057cfc0b0a4daa3a715b44d9c8
Christos Papazafeiropoulos <xpapazaf@gmail.com>
2017-04-03 12:26:56 +0200

    added proper mui theme. storybook works ok now.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RegisterForm from './containers/RegisterForm';
- 
+
 const App = () => (
-  <MuiThemeProvider>
     <RegisterForm />
-  </MuiThemeProvider>
 );
- 
+
 ReactDOM.render(
   <App />,
   document.getElementById('app')
--- src/containers/RegisterForm.js
+++ src/containers/RegisterForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './RegisterForm.css';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
@@ -9,57 +11,65 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 
 class RegisterForm extends React.Component {
-	constructor(props) {
-		super(props);
-        this.state = { name: this.props.name || '', surname: '', thankYouDialog: false };
-    
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.handleSurnameChange = this.handleSurnameChange.bind(this);
-        this.handleRegister = this.handleRegister.bind(this);
-        this.handleDialogClose = this.handleDialogClose.bind(this);     
-	}
+  constructor(props) {
+    super(props);
+    this.state = { name: this.props.name || '', surname: '', thankYouDialog: false };
 
-    handleNameChange(event, newValue) { this.setState({name: newValue});}
-    handleSurnameChange(event, newValue) { this.setState({surname: newValue});}
-    handleRegister(event) { this.setState({thankYouDialog: true}); }
-    handleDialogClose(event) { this.setState({thankYouDialog: false, name: '', surname: ''}); }
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSurnameChange = this.handleSurnameChange.bind(this);
+    this.handleRegister = this.handleRegister.bind(this);
+    this.handleDialogClose = this.handleDialogClose.bind(this);
+  }
 
-	render() {
-        const actions = [
-          <FlatButton
-            id = "ok-dialog-button"
-            label="OK"
-            primary={true}
-            keyboardFocused={true}
-            onTouchTap={this.handleDialogClose.bind(this)}
-          />,
-        ];
-		return (
-            <div className="registerForm">
-                <Paper className="paperStyle" zDepth={3}>
-                    <div className="inPaperStyle">
-                        <h3>Register Form</h3>
-                        <br />
-                        <TextField id="name-field" hintText="Name" onChange={this.handleNameChange} value={this.state.name}/><br />
-                        <TextField id="surname-field" hintText="Surname" onChange={this.handleSurnameChange}/><br />
-                        <br />
-                        <RaisedButton id="register-button" className="registerButton" label="Register" onTouchTap={this.handleRegister} />
-                    </div>
-                </Paper>
-                <Dialog
-                  id="thank-you-dialog"
-                  title="Registered"
-                  actions={actions}
-                  modal={false}
-                  open={this.state.thankYouDialog}
-                  onRequestClose={this.handleDialogClose}
-                >
-                  Thank you for registering {this.state.name} {this.state.surname}!
-                </Dialog>
-		    </div>
-        );
-	}
+  getChildContext() {
+    return { muiTheme: getMuiTheme(baseTheme) };
+  }
+
+  handleNameChange(event, newValue) { this.setState({name: newValue});}
+  handleSurnameChange(event, newValue) { this.setState({surname: newValue});}
+  handleRegister(event) { this.setState({thankYouDialog: true}); }
+  handleDialogClose(event) { this.setState({thankYouDialog: false, name: '', surname: ''}); }
+
+  render() {
+    const actions = [
+      <FlatButton
+        id = "ok-dialog-button"
+        label="OK"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleDialogClose.bind(this)}
+      />,
+    ];
+    return (
+        <div className="registerForm">
+          <Paper className="paperStyle" zDepth={3}>
+            <div className="inPaperStyle">
+              <h3>Register Form</h3>
+              <br />
+              <TextField id="name-field" hintText="Name" onChange={this.handleNameChange} value={this.state.name}/><br />
+              <TextField id="surname-field" hintText="Surname" onChange={this.handleSurnameChange}/><br />
+              <br />
+              <RaisedButton id="register-button" className="registerButton" label="Register" onTouchTap={this.handleRegister} />
+            </div>
+          </Paper>
+          <Dialog
+            id="thank-you-dialog"
+            title="Registered"
+            actions={actions}
+            modal={false}
+            open={this.state.thankYouDialog}
+            onRequestClose={this.handleDialogClose}
+          >
+          Thank you for registering {this.state.name} {this.state.surname}!
+          </Dialog>
+        </div>
+    );
+  }
 
 }
+
+RegisterForm.childContextTypes = {
+  muiTheme: React.PropTypes.object.isRequired,
+};
 
 export default RegisterForm;
